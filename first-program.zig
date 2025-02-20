ff71648dab446b2b3064d220c6151dd51bf9dba2
kittycat <kittynekostudio@gmail.com>
2025-02-20 11:14:45 -0800

    First program written with zig-kilo

---
+++ first-program.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+pub fn main() !void {
+	std.debug.print("First program written with zig-kilo!\n", .{});
+	std.debug.print("Not having backspace or enter is really annoying.\n", .{});
+}
